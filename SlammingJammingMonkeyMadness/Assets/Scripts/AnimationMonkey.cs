4be4acc8bceea988acd14d8e4b78f138c77268d7
ThamosPrinZzz <97098194@st.deltion.nl>
2023-04-14 12:38:12 +0200

    Animation Monkey

--- SlammingJammingMonkeyMadness/Assets/Scripts/AnimationMonkey.cs
+++ SlammingJammingMonkeyMadness/Assets/Scripts/AnimationMonkey.cs
@@ -15,10 +15,7 @@ public class AnimationMonkey : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            anim.SetTrigger("DoWalking");
-        }
-        
+        anim.SetFloat("vertical", Input.GetAxis("Vertical"));
+        anim.SetFloat("horizontal", Input.GetAxis("Horizontal"));
     }
 }
